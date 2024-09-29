014a5dd053225d40e701a34691942b0edf0fef3c
ArielSantos <arielsantos.ac@gmail.com>
2024-09-29 09:12:37 -0300

    feat: refac plugins

---
+++ config/plugins.js
@@ -0,0 +1,34 @@
+
+const HapiSwagger = require('hapi-swagger');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const Pack = require('./../package');
+const MongoosePlugin = require('./../_plugins/mongoose.plugin');
+const CompanyModule = require('./../_modules/company/company.module');
+const ApplicationModule = require('./../_modules/application/application.module');
+const CandidateModule = require('./../_modules/candidate/candidate.module');
+const JobModule = require('./../_modules/job/job.module');
+
+module.exports = [
+    {
+        plugin: MongoosePlugin,
+        options: {
+            mongoDbUri: 'mongodb://localhost:27017/hapi_db'
+        }
+    },
+    Inert,
+    Vision,
+    {
+        plugin: HapiSwagger,
+        options: {
+            info: {
+                title: 'Test API Documentation',
+                version: Pack.version,
+            },
+        }
+    },
+    CompanyModule,
+    ApplicationModule,
+    CandidateModule,
+    JobModule
+];
--- server.js
+++ server.js
@@ -1,17 +1,6 @@
 'use strict'
 const Hapi = require('@hapi/hapi');
-const mongoose = require('mongoose');
-const HapiSwagger = require('hapi-swagger');
-const Inert = require('@hapi/inert');
-const Vision = require('@hapi/vision');
-const Pack = require('./package');
-const MongoosePlugin = require('./_plugins/mongoose.plugin');
-
-// const companyRoutes = require('./_controllers/company.routes');
-const CompanyModule = require('./_modules/company/company.module');
-const ApplicationModule = require('./_modules/application/application.module');
-const CandidateModule = require('./_modules/candidate/candidate.module');
-const JobModule = require('./_modules/job/job.module');
+const plugins = require('./config/plugins');
 
 const init = async () => {
     const server = Hapi.server({
@@ -19,33 +8,7 @@ const init = async () => {
         host: 'localhost'
     });
 
-    // ROUTES
-    // server.route(companyRoutes);
-
-    // PLUGINS
-    await server.register([
-        {
-            plugin: MongoosePlugin,
-            options: {
-                mongoDbUri: 'mongodb://localhost:27017/hapi_db'
-            }
-        },
-        Inert,
-        Vision,
-        {
-            plugin: HapiSwagger,
-            options: {
-                info: {
-                    title: 'Test API Documentation',
-                    version: Pack.version,
-                },
-            }
-        },
-        CompanyModule,
-        ApplicationModule,
-        CandidateModule,
-        JobModule
-    ]);
+    await server.register(plugins);
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
